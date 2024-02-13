3d460496b9868b2bf7c857f098c194205d644b30
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-14 01:44:09 +0700

    created header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <header className="app-header">
+            <h1 className="app-header__title">PG APP</h1>
+        </header>
+    );
+}
