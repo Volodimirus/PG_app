dd52b289cc0c210b4f376cf279025408f5d63360
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-14 20:25:58 +0700

    created section for portfolio generator form

---
+++ src/components/pgForm/pgFormSection/PgFormSection.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./PgFormSection.css";
+import PgFormSectionField from "./pgFormSectionField/PgFormSectionField";
+
+export default function PgFormSection() {
+    return (
+        <fieldset className="pg-form_section">
+            <legend className="pg-form_section__title">
+                personal information
+            </legend>
+            <PgFormSectionField
+                type="text"
+                id="lslqzcmc"
+                name="firstName"
+                placeholder="First Name"
+            />
+            <PgFormSectionField
+                type="text"
+                id="lslqzcmd"
+                name="lastName"
+                placeholder="Last Name"
+            />
+            <PgFormSectionField
+                type="text"
+                id="lslqzcme"
+                name="jobTitle"
+                placeholder="Job Title"
+            />
+            <PgFormSectionField
+                type="url"
+                id="lslqzcmf"
+                name="photo"
+                placeholder="Your photo (URL)"
+            />
+            <PgFormSectionField
+                type="text"
+                id="lslqzcmg"
+                name="address"
+                placeholder="Address"
+            />
+            <PgFormSectionField
+                type="tel"
+                id="lslqzcmh"
+                name="phone"
+                placeholder="Phone Number"
+            />
+            <PgFormSectionField
+                type="email"
+                id="lslqzcmi"
+                name="email"
+                placeholder="Email"
+            />
+            <PgFormSectionField
+                type="textarea"
+                id="lslqzcmj"
+                name="about"
+                placeholder="About yourself"
+            />
+        </fieldset>
+    );
+}
