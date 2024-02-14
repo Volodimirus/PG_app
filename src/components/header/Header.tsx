f6c5437a201622ddcd46f49320d2078b1fe40a66
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-15 03:43:35 +0700

    update styles (added Bootstrap)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,8 +3,8 @@ import "./Header.css";
 
 export default function Header() {
     return (
-        <header className="app-header">
-            <h1 className="app-header__title">PG APP</h1>
+        <header className="app_header container-fluid d-flex align-items-center justify-content-center">
+            <h1 className="app_title fw-bolder">PG APP</h1>
         </header>
     );
 }
