a1496832482465d6f43f48faabc47f69e84364d6
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-17 06:11:11 +0700

    added Footer component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "../header/Header";
 import PgForm from "../pgForm/PgForm";
 import Portfolio from "../portfolio/Portfolio";
+import Footer from "../footer/Footer";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Header />
             <PgForm />
             <Portfolio />
+            <Footer />
         </div>
     );
 }
