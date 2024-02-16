c47456c37a201b483d615a4fb57c24cd7f4a4b5b
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-16 14:49:23 +0700

    added portfolio component to site

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "../header/Header";
 import PgForm from "../pgForm/PgForm";
+import Portfolio from "../portfolio/Portfolio";
 
 function App() {
     return (
-        <div className="app">
+        <div className="app min-vh-100 d-flex flex-column align-items-center gap-5">
             <Header />
             <PgForm />
+            <Portfolio />
         </div>
     );
 }
