490dc46689ff45003971949446173174af881652
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-17 17:29:48 +0700

    added types & added documentation

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Footer.css";
 
-export default function Footer() {
+/**
+ * Renders the footer.
+ * @returns The Footer component.
+ */
+
+export default function Footer(): JSX.Element {
     return (
         <footer className="app_footer w-100 py-3 d-flex justify-content-center align-items-center gap-3">
             <span className="app_footer__copyright">
