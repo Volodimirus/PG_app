1270ed82d091c18bb04aca1953817a852c598e8c
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-18 03:44:35 +0700

    added prop types & updated documentation & transfer personal details state to children components

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -2,19 +2,43 @@ import React from "react";
 import PortfolioHeader from "./portfolioHeader/PortfolioHeader";
 import PortfolioMain from "./portfolioMain/PortfolioMain";
 import PortfolioAside from "./portfolioAside/PortfolioAside";
+import { PortfolioProps } from "./PortfolioProps";
 import "./Portfolio.css";
 
 /**
  * Renders the portfolio page.
+ * @param props the portfolio props including the personal user details
+ * (first name, second name, email, job title, phone, photo, about).
  * @returns The portfolio page component.
  */
 
-export default function Portfolio() {
+export default function Portfolio(props: PortfolioProps): JSX.Element {
+    const { personalDetails } = props;
+    const {
+        firstName,
+        secondName,
+        jobTitle,
+        photo,
+        address,
+        phone,
+        email,
+        about,
+    } = personalDetails;
+
     return (
         <div className="portfolio w-100 d-grid">
-            <PortfolioHeader />
-            <PortfolioMain />
-            <PortfolioAside />
+            <PortfolioHeader
+                firstName={firstName}
+                secondName={secondName}
+                jobTitle={jobTitle}
+            />
+            <PortfolioMain about={about} />
+            <PortfolioAside
+                address={address}
+                phone={phone}
+                email={email}
+                photo={photo}
+            />
         </div>
     );
 }
