a2174dde9ac0353b469c63994420c90f9fcc4f36
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-17 13:37:19 +0700

    added documentation & formated documnet & added styles

--- src/components/pgForm/PgForm.tsx
+++ src/components/pgForm/PgForm.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import "./PgForm.css";
 import PgFormSection from "./pgFormSection/PgFormSection";
+import "./PgForm.css";
 
-export default function PgForm() {
+/**
+ * Renders the PgForm component.
+ * @returns The PgForm component.
+ */
+
+export default function PgForm(): JSX.Element {
     return (
         <form name="pg" className="pg-form w-100 shadow-lg d-flex flex-column">
             <PgFormSection />
