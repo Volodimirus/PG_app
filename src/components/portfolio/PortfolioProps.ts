8058510e906b69e8c955dfc2fae7a3875dc86a12
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-18 03:33:30 +0700

    interface for Portfolio props

---
+++ src/components/portfolio/PortfolioProps.ts
@@ -0,0 +1,12 @@
+export interface PortfolioProps {
+    personalDetails: {
+        firstName: string;
+        secondName: string;
+        email: string;
+        jobTitle: string;
+        photo: string;
+        address: string;
+        phone: string;
+        about: string;
+    };
+}
