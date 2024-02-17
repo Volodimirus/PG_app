bb462061b0b8ada54d5131b72c5819a88503fffd
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-18 03:30:10 +0700

    added personal details state & created functions to set state & transfer state to children components

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../header/Header";
 import PgForm from "../pgForm/PgForm";
 import Portfolio from "../portfolio/Portfolio";
@@ -11,11 +11,48 @@ import "./App.css";
  */
 
 function App(): JSX.Element {
+    const [personalDetails, setPersonalDetails] = useState({
+        firstName: "",
+        secondName: "",
+        jobTitle: "",
+        photo: "",
+        address: "",
+        phone: "",
+        email: "",
+        about: "",
+    });
+
+    const setFirstName = (firstName: string) =>
+        setPersonalDetails({ ...personalDetails, firstName });
+    const setSecondName = (secondName: string) =>
+        setPersonalDetails({ ...personalDetails, secondName });
+    const setJobTitle = (jobTitle: string) =>
+        setPersonalDetails({ ...personalDetails, jobTitle });
+    const setPhoto = (photo: string) =>
+        setPersonalDetails({ ...personalDetails, photo });
+    const setAddress = (address: string) =>
+        setPersonalDetails({ ...personalDetails, address });
+    const setPhone = (phone: string) =>
+        setPersonalDetails({ ...personalDetails, phone });
+    const setEmail = (email: string) =>
+        setPersonalDetails({ ...personalDetails, email });
+    const setAbout = (about: string) =>
+        setPersonalDetails({ ...personalDetails, about });
+
     return (
         <div className="app min-vh-100 d-flex flex-column align-items-center gap-5">
             <Header />
-            <PgForm />
-            <Portfolio />
+            <PgForm
+                setFirstName={setFirstName}
+                setSecondName={setSecondName}
+                setJobTitle={setJobTitle}
+                setPhoto={setPhoto}
+                setAddress={setAddress}
+                setPhone={setPhone}
+                setEmail={setEmail}
+                setAbout={setAbout}
+            />
+            <Portfolio personalDetails={personalDetails} />
             <Footer />
         </div>
     );
