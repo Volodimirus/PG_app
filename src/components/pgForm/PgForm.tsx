5604b2ab4f449fb2cf8309fde4934b906d7ef20a
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-18 03:40:15 +0700

    added prop types & updated documentation & transfer state setter functions

--- src/components/pgForm/PgForm.tsx
+++ src/components/pgForm/PgForm.tsx
@@ -1,16 +1,38 @@
 import React from "react";
 import PgFormSection from "./pgFormSection/PgFormSection";
+import { PgFormProps } from "./PgFormProps";
 import "./PgForm.css";
 
 /**
  * Renders the PgForm component.
+ * @param props The properties of field, including its type, ID, name, placeholder and onChange func.
  * @returns The PgForm component.
  */
 
-export default function PgForm(): JSX.Element {
+export default function PgForm(props: PgFormProps): JSX.Element {
+    const {
+        setFirstName,
+        setSecondName,
+        setEmail,
+        setJobTitle,
+        setPhoto,
+        setAddress,
+        setPhone,
+        setAbout,
+    } = props;
+
     return (
         <form name="pg" className="pg-form w-100 shadow-lg d-flex flex-column">
-            <PgFormSection />
+            <PgFormSection
+                setFirstName={setFirstName}
+                setSecondName={setSecondName}
+                setEmail={setEmail}
+                setJobTitle={setJobTitle}
+                setPhoto={setPhoto}
+                setAddress={setAddress}
+                setPhone={setPhone}
+                setAbout={setAbout}
+            />
         </form>
     );
 }
