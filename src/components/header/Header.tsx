aa152184281bc75d7facc28fa8be38844934ce4f
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-17 13:33:20 +0700

    added documentation & added types

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Header.css";
 
-export default function Header() {
+/**
+ * Return an element that displays the application title.
+ * @return {JSX.Element} The application title.
+ */
+
+export default function Header(): JSX.Element {
     return (
         <header className="app_header container-fluid d-flex align-items-center justify-content-center">
             <h1 className="app_title fw-bolder">PG APP</h1>
