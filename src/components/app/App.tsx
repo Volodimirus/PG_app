3c2ddf89d4774c2cb9ab68abadd25269d9243cf7
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-17 13:30:45 +0700

    added types

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import "./App.css";
 import Header from "../header/Header";
 import PgForm from "../pgForm/PgForm";
 import Portfolio from "../portfolio/Portfolio";
 import Footer from "../footer/Footer";
+import "./App.css";
 
 // Renders the main application component.
 // @returns The main application component.
 
-function App() {
+function App(): JSX.Element {
     return (
         <div className="app min-vh-100 d-flex flex-column align-items-center gap-5">
             <Header />
