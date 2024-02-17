4868f6398ac27abd2c0e69816d9fae11b7dda307
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-18 03:32:49 +0700

    interface for PgForm props

---
+++ src/components/pgForm/PgFormProps.ts
@@ -0,0 +1,10 @@
+export interface PgFormProps {
+    setFirstName: (firstName: string) => void;
+    setSecondName: (secondName: string) => void;
+    setEmail: (email: string) => void;
+    setJobTitle: (jobTitle: string) => void;
+    setPhoto: (photo: string) => void;
+    setAddress: (address: string) => void;
+    setPhone: (phone: string) => void;
+    setAbout: (about: string) => void;
+}
