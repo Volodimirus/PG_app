e3020e207f75b6fade5c1176901ca3ccc66ae07f
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-21 04:30:18 +0700

    added experience slice to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import personalInfoReducer from "./reducers/personalInfoSlice";
+import experienceSlice from "./reducers/experienceSlice";
 
 export const store = configureStore({
     reducer: {
         personalInfo: personalInfoReducer,
+        experience: experienceSlice,
     },
 });
 
