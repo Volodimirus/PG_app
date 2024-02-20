7b3264cd932d512d3542786c13f8050c05d93210
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-21 02:50:06 +0700

    added personal details reducer

---
+++ src/redux/reducers/personalInfoSlice.ts
@@ -0,0 +1,64 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction } from "@reduxjs/toolkit";
+
+export interface PersonalInfoState {
+    firstName: string;
+    secondName: string;
+    jobTitle: string;
+    photo: string;
+    address: string;
+    phone: string;
+    email: string;
+    about: string;
+}
+
+const initialState: PersonalInfoState = {
+    firstName: "",
+    secondName: "",
+    jobTitle: "",
+    photo: "",
+    address: "",
+    phone: "",
+    email: "",
+    about: "",
+};
+
+export const personalInfoSlice = createSlice({
+    name: "personalInfo",
+    initialState,
+    reducers: {
+        setFirstName: (state, action: PayloadAction<string>) => {
+            state.firstName = action.payload;
+        },
+        setSecondName: (state, action: PayloadAction<string>) => {
+            state.secondName = action.payload;
+        },
+        setJobTitle: (state, action: PayloadAction<string>) => {
+            state.jobTitle = action.payload;
+        },
+        setPhoto: (state, action: PayloadAction<string>) => {
+            state.photo = action.payload;
+        },
+        setAddress: (state, action: PayloadAction<string>) => {
+            state.address = action.payload;
+        },
+        setPhone: (state, action: PayloadAction<string>) => {
+            state.phone = action.payload;
+        },
+        setEmail: (state, action: PayloadAction<string>) => {
+            state.email = action.payload;
+        },
+    },
+});
+
+export const {
+    setFirstName,
+    setSecondName,
+    setJobTitle,
+    setPhoto,
+    setAddress,
+    setPhone,
+    setEmail,
+} = personalInfoSlice.actions;
+
+export default personalInfoSlice.reducer;
