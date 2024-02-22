c8bfc0abc4a58f44591eff5910315b3e8b9e97b6
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-22 20:08:35 +0700

    created education info slice'

---
+++ src/redux/reducers/educationSlice.ts
@@ -0,0 +1,63 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+/**
+ * Interface for the state of the experience slice.
+ */
+export interface EducationState {
+    education: [EducationItemState?];
+}
+
+/**
+ * Interface for the state of the education item.
+ */
+export interface EducationItemState {
+    univercity: string;
+    city: string;
+    degree: string;
+    subject: string;
+    fromYear: number;
+    toYear: number;
+}
+
+/**
+ * The initial state of the experience slice.
+ */
+const initialState: EducationState = {
+    education: [],
+};
+
+/**
+ * Creates the experience slice.
+ */
+const educationSlice = createSlice({
+    name: "education",
+    initialState,
+    reducers: {
+        /**
+         * Adds an experience item to the state.
+         * @param state The current state.
+         * @param action The action payload containing the education item.
+         */
+        addEducation: (state, action: PayloadAction<EducationItemState>) => {
+            state.education.push(action.payload);
+        },
+        /**
+         * Deletes an experience item from the state.
+         * @param state The current state.
+         * @param action The action payload containing the index of the item to delete.
+         */
+        deleteEducation: (state, action: PayloadAction<number>) => {
+            state.education.splice(action.payload, 1);
+        },
+    },
+});
+
+/**
+ * The actions for the experience slice.
+ */
+export const { addEducation, deleteEducation } = educationSlice.actions;
+
+/**
+ * The reducer for the experience slice.
+ */
+export default educationSlice.reducer;
