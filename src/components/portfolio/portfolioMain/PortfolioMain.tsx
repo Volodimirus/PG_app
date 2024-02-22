7d728468635ec83b902fbca1e8c51c8ad8173544
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-22 09:51:15 +0700

    update styles && rendering experience items

--- src/components/portfolio/portfolioMain/PortfolioMain.tsx
+++ src/components/portfolio/portfolioMain/PortfolioMain.tsx
@@ -10,6 +10,7 @@ import "./PortfolioMain.css";
 
 export default function PortfolioMain(): JSX.Element {
     const { about } = useSelector((state: RootState) => state.personalInfo);
+    const { experience } = useSelector((state: RootState) => state.experience);
 
     return (
         <div className="portfolio_main d-flex flex-column">
@@ -25,7 +26,30 @@ export default function PortfolioMain(): JSX.Element {
                 <h3 className="portfolio_main__title border-bottom fs-4 lh-lg">
                     Experience
                 </h3>
-                <span className="portfolio_main__content">-</span>
+                <span className="portfolio_main__content">
+                    {experience.length
+                        ? experience.map((item, i: number) => {
+                              return (
+                                  <div
+                                      className="experience d-flex gap-5"
+                                      key={i}
+                                  >
+                                      <p className="experience__years fs-5 lh">
+                                          {item?.fromYear} - {item?.toYear}
+                                      </p>
+                                      <div className="d-flex flex-column gap-2">
+                                          <h4 className="experience__job fs-5">
+                                              {item?.position}
+                                          </h4>
+                                          <p className="experience__city_and_company fs-5">
+                                              {item?.company}, {item?.city}
+                                          </p>
+                                      </div>
+                                  </div>
+                              );
+                          })
+                        : "-"}
+                </span>
             </div>
             <div className="portfolio_main__bottom d-flex flex-column">
                 <h3 className="portfolio_main__title border-bottom fs-4 lh-lg">
