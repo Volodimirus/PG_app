4ba9f085bbe754199d7cc79e3e7bd12b04122a44
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-22 20:10:32 +0700

    added education slice to store'

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import personalInfoReducer from "./reducers/personalInfoSlice";
 import experienceSlice from "./reducers/experienceSlice";
+import educationSlice from "./reducers/educationSlice";
 
 export const store = configureStore({
     reducer: {
         personalInfo: personalInfoReducer,
         experience: experienceSlice,
+        education: educationSlice,
     },
 });
 
