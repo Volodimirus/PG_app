a27e856b278f46c53fac169181f5e61b43836511
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-22 09:48:47 +0700

    update styles && updated logic

--- src/components/pgForm/experienceFormSection/ExperienceFormSection.tsx
+++ src/components/pgForm/experienceFormSection/ExperienceFormSection.tsx
@@ -1,57 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
 import FormSectionField from "../formSectionField/FormSectionField";
-import {
-    setPosition,
-    setCompany,
-    setCity,
-    setFromYear,
-    setToYear,
-} from "../../../redux/reducers/experienceSlice";
+import { setExperienceItem } from "../../../redux/reducers/experienceSlice";
+import { ExperienceItemState } from "../../../redux/reducers/experienceSlice";
+import { useDispatch } from "react-redux";
 import "./ExperienceFormSection.css";
 
 export default function ExperienceFormSection(): JSX.Element {
+    const dispatch = useDispatch();
+
+    const [experienceDetails, setExperienceDetails] =
+        useState<ExperienceItemState>({
+            position: "",
+            company: "",
+            city: "",
+            fromYear: 0,
+            toYear: 0,
+        });
+    const [isOpen, toggleOpen] = useState<boolean>(false);
+
     return (
         <fieldset className="experience d-flex flex-column">
             <legend className="fs-4 lh-lg">Experience</legend>
-            <FormSectionField
-                type="text"
-                name="position"
-                placeholder="Position"
-                id="751cfacc"
-                action={setPosition}
-            />
-            <FormSectionField
-                type="text"
-                name="company"
-                placeholder="Company"
-                id="4f245f8c"
-                action={setCompany}
-            />
-            <FormSectionField
-                type="text"
-                name="city"
-                placeholder="City"
-                id="aa4e795b"
-                action={setCity}
-            />
-            <FormSectionField
-                type="number"
-                name="fromYear"
-                placeholder="From (year)"
-                id="702cecf1"
-                action={setFromYear}
-            />
-            <FormSectionField
-                type="number"
-                name="toYear"
-                placeholder="To (year)"
-                id="495ebd24"
-                action={setToYear}
-            />
-            <button type="button" className="w-100 fs-4 px-3">
-                Delete
-            </button>
-            <button type="button" className="w-100 fs-4 px-3">
+            {isOpen ? (
+                <>
+                    <FormSectionField
+                        type="text"
+                        name="position"
+                        placeholder="Position"
+                        id="751cfacc"
+                        action={(value: string) => {
+                            setExperienceDetails({
+                                ...experienceDetails,
+                                position: value,
+                            });
+                        }}
+                    />
+                    <FormSectionField
+                        type="text"
+                        name="company"
+                        placeholder="Company"
+                        id="4f245f8c"
+                        action={(value: string) =>
+                            setExperienceDetails({
+                                ...experienceDetails,
+                                company: value,
+                            })
+                        }
+                    />
+                    <FormSectionField
+                        type="text"
+                        name="city"
+                        placeholder="City"
+                        id="aa4e795b"
+                        action={(value: string) =>
+                            setExperienceDetails({
+                                ...experienceDetails,
+                                city: value,
+                            })
+                        }
+                    />
+                    <FormSectionField
+                        type="number"
+                        name="fromYear"
+                        placeholder="From (year)"
+                        id="702cecf1"
+                        action={(value: string) =>
+                            setExperienceDetails({
+                                ...experienceDetails,
+                                fromYear: +value,
+                            })
+                        }
+                    />
+                    <FormSectionField
+                        type="number"
+                        name="toYear"
+                        placeholder="To (year)"
+                        id="495ebd24"
+                        action={(value: string) =>
+                            setExperienceDetails({
+                                ...experienceDetails,
+                                toYear: +value,
+                            })
+                        }
+                    />
+                    <button
+                        type="button"
+                        className="w-100 fs-4 px-3"
+                        onClick={() => {
+                            setExperienceDetails({
+                                position: "",
+                                company: "",
+                                city: "",
+                                fromYear: 0,
+                                toYear: 0,
+                            });
+
+                            toggleOpen(false);
+                        }}
+                    >
+                        Delete
+                    </button>
+                </>
+            ) : null}
+            <button
+                type="button"
+                className="w-100 fs-4 px-3"
+                onClick={() => {
+                    toggleOpen(true);
+
+                    if (
+                        experienceDetails.city &&
+                        experienceDetails.fromYear &&
+                        experienceDetails.toYear &&
+                        experienceDetails.position &&
+                        experienceDetails.city &&
+                        experienceDetails.company
+                    ) {
+                        dispatch(setExperienceItem(experienceDetails));
+
+                        toggleOpen(false);
+
+                        setExperienceDetails({
+                            position: "",
+                            company: "",
+                            city: "",
+                            fromYear: 0,
+                            toYear: 0,
+                        });
+                    }
+                }}
+            >
                 Add
             </button>
         </fieldset>
