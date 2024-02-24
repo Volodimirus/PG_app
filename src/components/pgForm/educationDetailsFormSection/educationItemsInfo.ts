5800ca31f722335b38c06613cad55c5ed5dbcd88
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-24 18:40:29 +0700

    transfered education items info to another file

---
+++ src/components/pgForm/educationDetailsFormSection/educationItemsInfo.ts
@@ -0,0 +1,8 @@
+export const educationItemsInfo = [
+    { type: "text", name: "university", placeholder: "University" },
+    { type: "text", name: "city", placeholder: "City" },
+    { type: "text", name: "degree", placeholder: "Degree" },
+    { type: "text", name: "subject", placeholder: "Subject" },
+    { type: "number", name: "fromYear", placeholder: "From (year)" },
+    { type: "number", name: "toYear", placeholder: "To (year)" },
+];
