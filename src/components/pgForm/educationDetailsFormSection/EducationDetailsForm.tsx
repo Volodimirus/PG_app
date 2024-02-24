9858960e860784b470256f5d4487a0f12578cac4
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-24 19:10:33 +0700

    format document && fix orphographic

--- src/components/pgForm/educationDetailsFormSection/EducationDetailsForm.tsx
+++ src/components/pgForm/educationDetailsFormSection/EducationDetailsForm.tsx
@@ -3,10 +3,11 @@ import { useDispatch } from "react-redux";
 import FormSectionField from "../formSectionField/FormSectionField";
 import { addEducation } from "../../../redux/reducers/educationSlice";
 import { EducationItemState } from "../../../redux/reducers/educationSlice";
+import { educationItemsInfo } from "./educationItemsInfo";
 
 export default function EducationDetailsFormSection() {
     const [educationDetails, setEducationDetails] = useState<EducationItemState>({
-        univercity: "",
+        university: "",
         degree: "",
         fromYear: 0,
         toYear: 0,
@@ -20,93 +21,39 @@ export default function EducationDetailsFormSection() {
     return (
         <fieldset className="pg_form__section education d-flex flex-column">
             <legend className="fs-4 lh-lg">Education</legend>
+            {isOpen ? educationItemsInfo.map(({ type, name, placeholder }) => {
+                return (
+                    <FormSectionField
+                        type={type}
+                        name={name}
+                        placeholder={placeholder}
+                        action={(value: string) => {
+                            setEducationDetails({
+                                ...educationDetails,
+                                [`${name}`]: value,
+                            });
+                        }}
+                    />)
+            }) : null}
             {isOpen ? (
-                <>
-                    <FormSectionField
-                        type="text"
-                        name="univercity"
-                        placeholder="University"
-                        action={(value: string) => {
-                            setEducationDetails({
-                                ...educationDetails,
-                                univercity: value,
-                            });
-                        }}
-                    />
-                    <FormSectionField
-                        type="text"
-                        name="degree"
-                        placeholder="Degree"
-                        action={(value: string) => {
-                            setEducationDetails({
-                                ...educationDetails,
-                                degree: value,
-                            });
-                        }}
-                    />
-                    <FormSectionField
-                        type="text"
-                        name="subject"
-                        placeholder="Subject"
-                        action={(value: string) => {
-                            setEducationDetails({
-                                ...educationDetails,
-                                subject: value,
-                            });
-                        }}
-                    />
-                    <FormSectionField
-                        type="text"
-                        name="city"
-                        placeholder="City"
-                        action={(value: string) => {
-                            setEducationDetails({
-                                ...educationDetails,
-                                city: value,
-                            });
-                        }}
-                    />
-                    <FormSectionField
-                        type="number"
-                        name="fromYear"
-                        placeholder="From (year)"
-                        action={(value: string) => {
-                            setEducationDetails({
-                                ...educationDetails,
-                                fromYear: +value,
-                            });
-                        }}
-                    />
-                    <FormSectionField
-                        type="number"
-                        name="toYear"
-                        placeholder="To (year)"
-                        action={(value: string) => {
-                            setEducationDetails({
-                                ...educationDetails,
-                                toYear: +value,
-                            });
-                        }}
-                    />
-                    <button
-                        type="button"
-                        className="w-100 fs-4 px-3"
-                        onClick={() => {
-                            setEducationDetails({
-                                univercity: "",
-                                degree: "",
-                                fromYear: 0,
-                                toYear: 0,
-                                city: "",
-                                subject: ""
-                            });
+                <button
+                    type="button"
+                    className="w-100 fs-4 px-3"
+                    onClick={() => {
+                        setEducationDetails({
+                            university: "",
+                            degree: "",
+                            city: "",
+                            subject: "",
+                            fromYear: 0,
+                            toYear: 0,
+                        });
 
-                            toggleOpen(false);
-                        }}
-                    >
-                        Delete
-                    </button>
-                </>
+                        toggleOpen(false);
+                    }}
+                >
+                    Delete
+                </button>
             ) : null}
             <button type="button" className="w-100 fs-4 px-3" onClick={() => {
                 toggleOpen(true);
@@ -117,14 +64,14 @@ export default function EducationDetailsFormSection() {
                     educationDetails.toYear &&
                     educationDetails.degree &&
                     educationDetails.subject &&
-                    educationDetails.univercity
+                    educationDetails.university
                 ) {
                     dispatch(addEducation(educationDetails));
 
                     toggleOpen(false);
 
                     setEducationDetails({
-                        univercity: "",
+                        university: "",
                         degree: "",
                         fromYear: 0,
                         toYear: 0,
@@ -134,6 +81,6 @@ export default function EducationDetailsFormSection() {
                 }
             }}
             >Add</button>
-        </fieldset>
+        </fieldset >
     )
 }
