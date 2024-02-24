b77407dbc94beaf8681836798de5a5433595e355
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-02-24 19:15:11 +0700

    format document

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../header/Header";
 import PgForm from "../pgForm/PgForm";
 import Portfolio from "../portfolio/Portfolio";
